c4d078b26483e15b6c81ecbbcd098c4ebf167e27
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-06 08:36:40 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Christopher Lamm on 12/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
